20355499bec61af5991bc0f379e1da96b12e34cc
Makan Azarshahy <oro1919@gmail.com>
2018-02-22 14:41:35 -0800

    Added server test for similar route.

--- spec/server.test.js
+++ spec/server.test.js
@@ -7,4 +7,16 @@ describe('Test the root path', () => {
       expect(response.statusCode).toBe(200);
     });
   });
+
+  test('It should get a response Hello World back', () => {
+    return request(app).get('/').then((response) => {
+      expect(response.text).toBe('Hello World');
+    });
+  });
+
+  test('It should get the response "YOU GOT SIMILAR" when calling the server with "/similar"', () => {
+    return request(app).get('/similar').then((response) => {
+      expect(response.text).toBe('YOU GOT SIMILAR');
+    });
+  });
 });
