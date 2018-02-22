570efcd38523319a9e3a81d6e7ecf4cdafdb3328
Makan Azarshahy <oro1919@gmail.com>
2018-02-22 14:41:59 -0800

    Added route for similar

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const { router } = require('./routes/similar');
 
 const app = express();
+
+app.use('/similar', router);
 
 app.get('/', (req, res) => res.status(200).send('Hello World'));
 
