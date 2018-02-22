517a45886a18f13ded4693f59cb9ed59cc1cbff0
Makan Azarshahy <oro1919@gmail.com>
2018-02-22 15:27:13 -0800

    Refactored tests to make AirBnB happy

--- spec/server.test.js
+++ spec/server.test.js
@@ -2,27 +2,19 @@ const request = require('supertest');
 const app = require('../server/app');
 
 describe('Test the root path', () => {
-  test('It should response the GET method', () => {
-    return request(app).get('/').then((response) => {
-      expect(response.statusCode).toBe(200);
-    });
-  });
+  test('It should response the GET method', () =>
+    request(app).get('/').then(response =>
+      expect(response.statusCode).toBe(200)));
 
-  test('It should get a response Hello World back', () => {
-    return request(app).get('/').then((response) => {
-      expect(response.text).toBe('Hello World');
-    });
-  });
+  test('It should get a response Hello World back', () =>
+    request(app).get('/').then(response =>
+      expect(response.text).toBe('Hello World')));
 
-  test('It should get the response "YOU GOT SIMILAR" when calling the server with "/similar"', () => {
-    return request(app).get('/similar').then((response) => {
-      expect(response.text).toBe('YOU GOT SIMILAR');
-    });
-  });
+  test('It should get the response "YOU GOT SIMILAR" when calling the server with "/similar"', () =>
+    request(app).get('/similar').then(response =>
+      expect(response.text).toBe('YOU GOT SIMILAR')));
 
-  test("It should send a 404 if the url doesn't exists", () => {
-    return request(app).get('/bort').then((response) => {
-      expect(response.statusCode).toBe(404);
-    });
-  })
+  test("It should send a 404 if the url doesn't exists", () =>
+    request(app).get('/bort').then(response =>
+      expect(response.statusCode).toBe(404)));
 });
