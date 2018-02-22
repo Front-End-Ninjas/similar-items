28ce8671c6d2bb0f34178a9a130aeb63a56b8b57
Makan Azarshahy <oro1919@gmail.com>
2018-02-22 11:47:38 -0800

    Added basic server

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello Wolrd'));
+
+app.listen(3000, () => console.log('Listening on PORT: 3000'));
