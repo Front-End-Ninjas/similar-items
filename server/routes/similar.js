4c389fff80f31a6359ccda19732243ac476e8112
Makan Azarshahy <oro1919@gmail.com>
2018-02-22 16:44:32 -0800

    Removed useless tests, and added better tests.

--- server/routes/similar.js
+++ server/routes/similar.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('YOU GOT SIMILAR');
+router.get('/:id/similar', (req, res) => {
+  console.log(req.params.id);
+  res.status(200).send();
 });
 
 module.exports = router;
--- spec/server.test.js
+++ spec/server.test.js
@@ -2,19 +2,17 @@ const request = require('supertest');
 const app = require('../server/app');
 
 describe('Test the root path', () => {
-  test('It should response the GET method', () =>
+  test('It should respond to the GET method', () =>
     request(app).get('/').then(response =>
       expect(response.statusCode).toBe(200)));
-
-  test('It should get a response Hello World back', () =>
-    request(app).get('/').then(response =>
-      expect(response.text).toBe('Hello World')));
-
-  test('It should get the response "YOU GOT SIMILAR" when calling the server with "/similar"', () =>
-    request(app).get('/similar').then(response =>
-      expect(response.text).toBe('YOU GOT SIMILAR')));
 
   test("It should send a 404 if the url doesn't exists", () =>
     request(app).get('/bort').then(response =>
       expect(response.statusCode).toBe(404)));
+});
+
+describe('Test the similar path', () => {
+  test('It should respond to the GET with url "/item/:id/similar"', () =>
+    request(app).get('/item/19/similar').then(response =>
+      expect(response.statusCode).toBe(200)));
 });
