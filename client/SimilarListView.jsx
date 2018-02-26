83377c92ab45c69ddb240d37504153379f0f1576
Makan Azarshahy <oro1919@gmail.com>
2018-02-26 09:46:00 -0800

    Added the skeleton for the similar board view.

--- client/SimilarListView.jsx
+++ client/SimilarListView.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import ThumbnailView from './ThumbnailView';
+import seed from '../seedData/seed.json';
 
-const SimilarListView = () => (
-  <div>
-    <h1>HELLO WORLD!</h1>
-  </div>
-);
+class SimilarListView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 0,
+      list: seed,
+      limit: 9,
+    };
+  }
+
+  leftClick() {
+
+  }
+
+  rightClick() {
+
+  }
+
+  render() {
+    console.log(this.state.list);
+
+    return (
+      <div className="list-container">
+        <div>
+          <span>Page {this.state.page} of {this.state.limit}</span>
+          <span> | </span>
+          <span>Start Over</span>
+        </div>
+        <div>
+          <div className="left">LEFT BUTTON</div>
+          <div className="thumbnail-container">
+            {
+              this.state.list.map(item => <ThumbnailView key={item.id} item={item} />)
+            }
+          </div>
+          <div className="right">RIGHT BUTTON</div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default SimilarListView;
