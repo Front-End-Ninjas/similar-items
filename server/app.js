ac012ddb7b7a316341618f165c63e41d44f1b9c0
Makan Azarshahy <oro1919@gmail.com>
2018-02-27 15:12:40 -0800

    Added assests route to get the prime icon and the arrow icons

--- server/app.js
+++ server/app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const path = require('path');
 const similar = require('./routes/similar');
 const image = require('./routes/images');
+const assets = require('./routes/assets');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'client'), { index: ['thumbnail.html'] }));
+
 app.use('/item', similar);
 app.use('/thumbnail', image);
+app.use('/assets', assets);
 
 module.exports = app;
