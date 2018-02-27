d780ae999a6abdc6fd2e0997c5af65ad6d6e1b5a
Makan Azarshahy <oro1919@gmail.com>
2018-02-27 15:14:30 -0800

    Added assests routes

---
+++ server/routes/assets.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const assets = express.Router();
+
+assets.get('*', (req, res) => {
+  const assetPath = path.join(__dirname, '..', '..', 'imgData', `${req.url}.png`);
+
+  fs.stat(assetPath, (err) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.sendFile(assetPath);
+    }
+  });
+});
+
+module.exports = assets;
