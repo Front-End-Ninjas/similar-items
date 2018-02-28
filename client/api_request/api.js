1bbaa9c9323e095c09d5f7b1378d695244cf2264
Makan Azarshahy <oro1919@gmail.com>
2018-02-28 14:59:56 -0800

    Added api helper function with axios

---
+++ client/api_request/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const search = (id, callback) => {
+  axios.get(`http://localhost:3000/item/${id}/similar`)
+    .then(list => callback(null, list))
+    .catch(err => callback(err, null));
+};
+
+export default search;
