53c177451b02523232c77611b262167c00266b9b
Makan Azarshahy <oro1919@gmail.com>
2018-02-28 11:06:51 -0800

    Added correct reponse for an item id.

--- server/routes/similar.js
+++ server/routes/similar.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const helpers = require('../db_helpers/helpers');
 
 const similar = express.Router();
 
 similar.get('/:id/similar', (req, res) => {
-  res.status(200).send('Success');
+  const { id } = req.params;
+
+  helpers.getList(id, (err, list) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.json(list);
+    }
+  });
 });
 
 module.exports = similar;
