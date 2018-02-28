44814e29eea2547ecc31b9378ff4381548d2767f
Makan Azarshahy <oro1919@gmail.com>
2018-02-28 15:28:03 -0800

    Refactored code

--- client/api_request/api.js
+++ client/api_request/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const search = (id, callback) => {
-  axios.get(`http://localhost:3000/item/${id}/similar`)
-    .then(list => callback(null, list))
-    .catch(err => callback(err, null));
-};
+const search = (id, callback) => axios
+  .get(`http://localhost:3000/item/${id}/similar`)
+  .then(list => callback(null, list))
+  .catch(err => callback(err, null));
 
 export default search;
