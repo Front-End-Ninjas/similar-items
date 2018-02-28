34c497d8a6649d337949c560ea4334776394c4bd
Makan Azarshahy <oro1919@gmail.com>
2018-02-28 15:31:13 -0800

    Added tests for api call in server

--- spec/server.test.js
+++ spec/server.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../server/app');
+const client = require('../db/pgClient');
 
 describe('Test the root path', () => {
   test('It should respond to the GET method', () =>
@@ -12,10 +13,13 @@ describe('Test the root path', () => {
 });
 
 describe('Test the similar path', () => {
+  beforeAll(() => client.connect());
+
+  afterAll(() => client.end());
+
   test('It should respond to the GET with url "/item/:id/similar"', () =>
     request(app).get('/item/1/similar').then((response) => {
       const { body, statusCode } = response;
-
       expect(statusCode).toBe(200);
       expect(body.rows).toBeInstanceOf(Array);
       expect(body.rows[0].category).toBe('Movies');
